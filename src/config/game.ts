bb691ff426081247f5e89aed0a60399a7a373b19
AMK1215 <aungmyokhaing1215@gmail.com>
2025-05-15 09:43:38 +0630

    gsc integrate

--- src/config/game.ts
+++ src/config/game.ts
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const GAME_OPERATOR_CODE = process.env.GAME_OPERATOR_CODE || '';
-export const GAME_SECRET_KEY = process.env.GAME_API_SECRET_KEY || '';
+export const GAME_SECRET_KEY = process.env.GAME_SECRET_KEY || '';
 export const GAME_API_URL = process.env.GAME_API_URL || ''; 
